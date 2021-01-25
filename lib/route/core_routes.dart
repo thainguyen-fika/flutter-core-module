bbfa0b43a58e51d67fbe3bff48ce829bdd411501
Thai Nguyen <quocthai.nl@gmail.com>
2021-01-25 10:25:48 +0700

    CoreRoutes - first initialize navigatorKey

--- lib/route/core_routes.dart
+++ lib/route/core_routes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 
 abstract class CoreRouter {
   static FluroRouter router = FluroRouter();
-  static GlobalKey<NavigatorState> navigatorKey;
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   static const ENCODE_OF_ROUTER_KEY = "\$!@#%^&*qazasdedcrfvbnhyukokm*&^%##@!\$";
 }
